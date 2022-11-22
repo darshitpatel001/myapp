8b09047ef62ea1ec5cc408a6e1383e8464f59317
darshitpatel001 <darshitakbari9999@gmail.com>
2022-11-22 16:40:46 +0600

    my

---
+++ src/Formcoll.js
@@ -0,0 +1,12 @@
+import React from 'react'
+
+export default function Formcoll() {
+    const [first, setfirst] = useState()
+
+    
+  return (
+    <div>
+
+    </div>
+  )
+}
--- src/Formdegree.js
+++ src/Formdegree.js
@@ -1,56 +1,58 @@
-import React from 'react'
-import Select from 'react-select';
+import React from "react";
+import Select from "react-select";
+import { useState } from "react";
 
 export default function Formdegree() {
-    const [data,setData] = useState({
-        name : '',
-        degree : '',
-        collage : ''
-    });
+  const [data, setData] = useState({
+    name: "",
+    Degree: "",
+    collage: "",
+  });
 
-    const Degree = [
-        { value: 'BCOM', label: 'BCOM' },
-        { value: 'MCA', label: 'MCA' },
-        { value: 'LLB', label: 'LLB' },
-        { value: 'IIT', label: 'IIT' },
-      ];
-      const Collage = [
-        { value: 'MSU', label: 'MSU' },
-        { value: 'PARUL', label: 'PARUL' },
-        { value: 'DHARUKA', label: 'DHARUKA' },
-        { value: 'RK', label: 'RK' },
-      ];
+  const Degree = [
+    { value: "BCOM", label: "BCOM" },
+    { value: "MCA", label: "MCA" },
+    { value: "LLB", label: "LLB" },
+    { value: "IIT", label: "IIT" },
+  ];
+  const Collage = [
+    { value: "MSU", label: "MSU" },
+    { value: "PARUL", label: "PARUL" },
+    { value: "DHARUKA", label: "DHARUKA" },
+    { value: "RK", label: "RK" },
+  ];
 
-      const Handler =(e)=> {
-        console.log(e);
-        setData({...data,[e.target.name] : e.target.value})
-      }
+  const Handler = (e) => {
+    console.log(e);
+    setData({ ...data, [e.target.name]: e.target.value });
+  };
 
-      const handleSubmit = (e)=> {
-        e.preventDefault();
-        console.log(data);
-      }
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    console.log(data);
+  };
 
   return (
     <div>
-  <form onSubmit={handleSubmit}>
-        <input type="Text" name='name' onChange={Handler}/>
-      
+      <form onSubmit={handleSubmit}>
+        Name  : <input type="Text" name="name" onChange={Handler} />
+
         <Select
-               options={Collage}
-        onChange={(e)=> {
-            handler({target:{name : 'collage', value: e.value}}) }}
-        name='collage'
-      />
+          options={Collage}
+          onChange={(e) => {
+            Handler({ target: { name: "collage", value: e.value } });
+          }}
+          name="collage"
+        />
         <Select
-        options={Degree}
-       onChange={(e)=> {
-        handler({target:{name : 'Degree', value: e.value}}) }}
-        name='Degree'
-       
-      />
-      <input type="submit" value="Submit"/>
+          options={Degree}
+          onChange={(e) => {
+            Handler({ target: { name: "Degree", value: e.value } });
+          }}
+          name="Degree"
+        />
+        <input type="submit" value="Submit" />
       </form>
     </div>
-  )
+  );
 }
